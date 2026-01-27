cd63ccbef3ba565a5eab653bde8a08344fde504c
Mark Wheeler <mhwheeler@gmail.com>
2026-01-27 10:26:37 -0800

    don't load inject.js (not used because no actual API calling)

--- feed-page/inject.js
+++ feed-page/inject.js
@@ -1,3 +1,16 @@
+// This script was injected into the actual page load and can interact with the actual page
+// It was used to find the SC User ID through actual page content.
+// It us not currently used as the extension does not currently use the API
+// To restore - put this in the manifest.json...
+// "content_scripts": [
+//   {
+//     "js": ["feed-page/inject.js"],
+//     "matches": ["https://soundcloud.com/*"],
+//     "run_at": "document_end",
+//     "world": "MAIN"
+//   },
+// ]
+
 console.log('inject.js loaded');
 
 function findSCUserId() {
